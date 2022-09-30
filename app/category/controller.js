45ea70ce1a994371e63ceed88a8435925e6c857e
AnaMelisa1992 <annmlsaaa@gmail.com>
2022-09-30 10:45:42 +0700

    Upload pertama

---
+++ app/category/controller.js
@@ -0,0 +1,17 @@
+module.exports={
+    index: async(req, res)=>{
+        try {
+            res.render('admin/category/view_category')
+         }catch (err) {
+            console.log(err)
+
+        }
+    },
+    viewCreate : async(req, res)=>{
+        try{
+        res.render('admin/category/create')
+        }catch (err) {
+            console.log(err)
+        }
+    }
+}
